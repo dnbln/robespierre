14cc62a948850778391b5c97a10c1b0cfb48722f
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2021-09-09 14:09:14 +0300

    match single arm => if let  in example-low-level

--- examples/example-bot-lowlevel/src/main.rs
+++ examples/example-bot-lowlevel/src/main.rs
@@ -59,84 +59,81 @@ impl RawEventHandler for Handler {
     ) {
         event.commit_to_cache_ref(&ctx).await;
 
-        match event {
-            robespierre_models::events::ServerToClientEvent::Message { message } => {
-                if matches!(&message.content, MessageContent::Content(s) if s == "Hello") {
-                    let author = ctx.0.fetch_user(message.author).await.unwrap();
-                    let channel = ctx.0.fetch_channel(message.channel).await.unwrap();
-                    let server = if let Some(server) = channel.server_id() {
-                        Some(ctx.0.fetch_server(server).await.unwrap())
-                    } else {
-                        None
-                    };
+        if let robespierre_models::events::ServerToClientEvent::Message { message } = event {
+            if matches!(&message.content, MessageContent::Content(s) if s == "Hello") {
+                let author = ctx.0.fetch_user(message.author).await.unwrap();
+                let channel = ctx.0.fetch_channel(message.channel).await.unwrap();
+                let server = if let Some(server) = channel.server_id() {
+                    Some(ctx.0.fetch_server(server).await.unwrap())
+                } else {
+                    None
+                };
 
-                    // let _session = message.channel.start_typing(ctx);
-                    // tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+                // let _session = message.channel.start_typing(ctx);
+                // tokio::time::sleep(std::time::Duration::from_secs(10)).await;
 
-                    let att_id = ctx
-                        .0
-                        .upload_autumn(
-                            AutumnTag::Attachments,
-                            "help".to_string(),
-                            "help me".to_string().into_bytes(),
-                        )
-                        .await
-                        .unwrap();
+                let att_id = ctx
+                    .0
+                    .upload_autumn(
+                        AutumnTag::Attachments,
+                        "help".to_string(),
+                        "help me".to_string().into_bytes(),
+                    )
+                    .await
+                    .unwrap();
 
-                    let _ = ctx
-                        .0
-                        .send_message(
-                            message.channel,
-                            format!(
-                                "Hello <@{}> from <#{}>{}",
-                                author.id,
-                                channel.id(),
-                                server
-                                    .map_or_else(Default::default, |it| format!(" in {}", it.name))
-                            ),
-                            rusty_ulid::generate_ulid_string(),
-                            vec![att_id],
-                            vec![ReplyData {
-                                id: message.id,
-                                mention: true,
-                            }],
-                        )
-                        .await;
-                }
-
-                // framework commands
-                if matches!(&message.content, MessageContent::Content(s) if s == "!ping" || s == "!pong") {
-                    let num = ctx.2.fetch_add(1, Ordering::Relaxed);
-
-                    let _ = ctx
-                        .0
-                        .send_message(
-                            message.channel,
-                            "Who pinged me?!",
-                            rusty_ulid::generate_ulid_string(),
-                            vec![],
-                            vec![ReplyData {
-                                id: message.id,
-                                mention: false,
-                            }],
-                        )
-                        .await;
-                    let _ = ctx
-                        .0
-                        .send_message(
-                            message.channel,
-                            format!("I got {} pings since I came online", num),
-                            rusty_ulid::generate_ulid_string(),
-                            vec![],
-                            vec![ReplyData {
-                                id: message.id,
-                                mention: false,
-                            }],
-                        )
-                        .await;
-                }
+                let _ = ctx
+                    .0
+                    .send_message(
+                        message.channel,
+                        format!(
+                            "Hello <@{}> from <#{}>{}",
+                            author.id,
+                            channel.id(),
+                            server
+                                .map_or_else(Default::default, |it| format!(" in {}", it.name))
+                        ),
+                        rusty_ulid::generate_ulid_string(),
+                        vec![att_id],
+                        vec![ReplyData {
+                            id: message.id,
+                            mention: true,
+                        }],
+                    )
+                    .await;
             }
-            _ => {}
+
+            // framework commands
+            if matches!(&message.content, MessageContent::Content(s) if s == "!ping" || s == "!pong") {
+                let num = ctx.2.fetch_add(1, Ordering::Relaxed);
+
+                let _ = ctx
+                    .0
+                    .send_message(
+                        message.channel,
+                        "Who pinged me?!",
+                        rusty_ulid::generate_ulid_string(),
+                        vec![],
+                        vec![ReplyData {
+                            id: message.id,
+                            mention: false,
+                        }],
+                    )
+                    .await;
+                let _ = ctx
+                    .0
+                    .send_message(
+                        message.channel,
+                        format!("I got {} pings since I came online", num),
+                        rusty_ulid::generate_ulid_string(),
+                        vec![],
+                        vec![ReplyData {
+                            id: message.id,
+                            mention: false,
+                        }],
+                    )
+                    .await;
+            }
         }
     }
 }
